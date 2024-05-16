fa2a070c550eb708c615116df65c53dfc10f64de
hoony99 <hoony0928@sju.ac.kr>
2024-05-16 19:06:07 +0900

    front통신
    비디오, 링크 분리
    프론트엔드와 통신연결

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
-from flask import Flask, request, render_template, redirect, url_for, flash
-#from flask_cors import CORS
-#from flask_jwt_extended import *
+from flask import Flask, request, render_template, redirect, url_for, flash, jsonify
+from flask_cors import CORS
+from flask_jwt_extended import *
 from werkzeug.utils import secure_filename
 import os
 import subprocess
@@ -23,12 +23,13 @@ import json
 from datetime import datetime
 
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = os.urandom(24).hex()
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['ALLOWED_EXTENSIONS'] = {'mov', 'mp4', 'avi'}
 
 # JWT 매니저 설정
-#jwt = JWTManager(app)
+jwt = JWTManager(app)
 
 # 허용된 파일 확장자 검사
 def allowed_file(filename):
@@ -45,6 +46,7 @@ def load_model():
     model.eval()
     return model, device
 
+# 도분초 방식의 gps 좌표를 십진수 방식으로 변환
 def dms_to_decimal(dms):
     # 정규 표현식을 사용하여 DMS 형식을 추출
     match = re.match(r'(\d+) deg (\d+)\u0027 (\d+\.\d+)" ([NSEW])', dms)
@@ -58,7 +60,7 @@ def dms_to_decimal(dms):
     if direction in ['S', 'W']:
         decimal *= -1    
     return decimal
-
+# 동영상 파일에서 gps추출
 def extract_gps_data(video_path):
     # GPS 정보 추출을 위한 ExifTool 명령어
     command = ['exiftool', '-GPSLatitude', '-GPSLongitude', '-json', video_path]    
@@ -78,35 +80,15 @@ def extract_gps_data(video_path):
     print("No GPS data available in the video.")
     return 0, 0
 
-# 비디오 링크 업로드 라우트
-# @app.route('/api/v1/public/upload-video', methods=['GET', 'POST'])
-@app.route('/', methods=['GET', 'POST'])
-def upload_file_or_link():
-    if request.method == 'POST':
-        file = request.files.get('file')
-        video_link = request.form.get('video_link')
-        
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-            gps_info = extract_gps_data(file_path)
-        elif video_link:
-            gps_info = request.form.get('gps_info', '')
-        else:
-            flash('No valid video input provided')        
-        return redirect(url_for('upload_file_or_link'))    
-    return render_template('upload.html')
-
 # database에 데이터 추가
 def insert_accident_data(accident_info):
     try:
         # 데이터베이스 연결 설정
         connection = mysql.connector.connect(
-            host='127.0.0.1',
-            database='capstone_ai',
-            user='ai_manager',
-            password='0000'
+            host='localhost',
+            database='ai_capstone',
+            user='root',
+            password='Abcd123@'
         )
         # 쿼리 실행을 위한 커서 생성
         cursor = connection.cursor()
@@ -130,45 +112,36 @@ def insert_accident_data(accident_info):
             connection.close()
             print("MySQL connection is closed")
 
-def sendData(accident_info):
-    # JSON 형식으로 변환
-    requestDtoJson = json.dumps(accident_info)
+# def sendData(accident_info):
+#     # JSON 형식으로 변환
+#     requestDtoJson = json.dumps(accident_info)
 
-    # 파일과 데이터를 멀티파트 폼 데이터로 전송
-    files = {
-        'image': ('accidnetImg.png', open(accident_info['imagePath'], 'rb'), 'image/png'),
-        'requestDto': (None, requestDtoJson, 'application/json')
-    }
-    # 자바 스프링 부트 서버의 URL (적절하게 수정 필요)
-    url = 'http://localhost:8080/api/accident/receiving-data'
-    # POST 요청 보내기
-    response = requests.post(url, files=files)
+#     # 파일과 데이터를 멀티파트 폼 데이터로 전송
+#     files = {
+#         'image': ('accidnetImg.png', open(accident_info['imagePath'], 'rb'), 'image/png'),
+#         'requestDto': (None, requestDtoJson, 'application/json')
+#     }
+#     # 자바 스프링 부트 서버의 URL (적절하게 수정 필요)
+#     url = 'http://localhost:8080/api/accident/receiving-data'
+#     # POST 요청 보내기
+#     response = requests.post(url, files=files)
 
-# 비디오 처리
-def process_video(source, model, device, gps_info):
-    cap = cv2.VideoCapture(source)
-    if not cap.isOpened():
-        return ['Failed to open video source']
-
-    fps = cap.get(cv2.CAP_PROP_FPS)
+def process_streaming_link(video_link, model, device, gps_info):
+    cap = cv2.VideoCapture(video_link)
     transform = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize((224, 224)),
-        transforms.ToTensor()
-    ])
-
+        transforms.ToPILImage(), 
+        transforms.Resize((224, 224)), 
+        transforms.ToTensor()])
     frame_count = 0
     results = []
     folder_path = 'C:\\Capston\\accident-detection-ai\\accident-detection-ai\\img'
     frame_times = []
     accident_count = 0
     frame_skip = 1  # 초기 frame_skip 값
-
     while cap.isOpened() and accident_count < 5:
         ret, frame = cap.read()
         if not ret:
-            break
-        
+            break        
         if frame_count % frame_skip == 0:
             input_tensor = transform(frame).unsqueeze(0).to(device)
             with torch.no_grad(): 
@@ -195,17 +168,114 @@ def process_video(source, model, device, gps_info):
                             "sort" : 'NULL',
                             "serverity" : 'NULL'
                         }
+                        #db에 내용추가
                         insert_accident_data(accident_info)
                         results.append(accident_info)
-                        sendData(accident_info)
+                        #backend로 데이터전송
+                        #sendData(accident_info)
                     frame_skip = 1  # 사고가 발생하면 다음 프레임 검사
                 else:
                     frame_skip = 5  # 사고가 없으면 5 프레임 후 검사        
         frame_count += 1
-
     cap.release()
     return results
 
+# 비디오 처리
+def process_video(source, model, device, gps_info):
+    cap = cv2.VideoCapture(source)
+    if not cap.isOpened():
+        return ['Failed to open video source']
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()
+    ])
+
+    frame_count = 0
+    results = []
+    folder_path = 'C:\\Capston\\accident-detection-ai\\accident-detection-ai\\img'
+    frame_times = []
+    accident_count = 0
+    frame_skip = 1  # 초기 frame_skip 값
+
+    while cap.isOpened() and accident_count < 5:
+        ret, frame = cap.read()
+        if not ret:
+            break        
+        if frame_count % frame_skip == 0:
+            input_tensor = transform(frame).unsqueeze(0).to(device)
+            with torch.no_grad(): 
+                output = model(input_tensor)
+                _, predicted = torch.max(output, 1)
+                accident = 0 if predicted.item() == 1 else 1.
+                if accident == 1: # 사고발생하면
+                    accident_count += 1
+                    if(accident_count == 5):
+                        #이미지 저장
+                        filename = f'accident_{datetime.now().strftime("%Y%m%d_%H%M%S")}.png'
+                        full_path = os.path.join(folder_path, filename)
+                        cv2.imwrite(full_path, frame)
+                        # 이미지 경로, 사고여부, GPS, 위도, 경도 정보 추가
+                        lat, lon = gps_info if gps_info != 'No GPS info available' else ('0', '0')
+                        accident_info = {
+                            "imagePath": full_path,
+                            "accident": accident,
+                            "latitude": lat,
+                            "longitude": lon,
+                            "date": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                            "sort" : 'NULL',
+                            "severity" : 'NULL'
+                        }
+                        #db에 내용추가
+                        insert_accident_data(accident_info)
+                        results.append(accident_info)
+                        #sendData(accident_info) #backend로 데이터전송
+                        return results # 영상은 백에 데이터 전송 후 바로 종료
+                    frame_skip = 1  # 사고가 발생하면 다음 프레임 검사
+                else:
+                    frame_skip = 5  # 사고가 없으면 5 프레임 후 검사        
+        frame_count += 1
+    cap.release()
+    return results
+
+# 비디오 링크 업로드 라우트
+@app.route('/api/v1/public/upload-link', methods=['GET', 'POST'])
+def upload_link():
+    if 'video_link' not in request.json:
+        return jsonify({'error': 'No video link provided'}), 400
+    video_link = request.json['video_link']
+    try:
+        model, device = load_model()
+        gps_info = request.form.get('gps_info', '')
+        results = process_streaming_link(video_link, model, device, gps_info)
+        return results
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+# 비디오 파일 업로드 라우트
+@app.route('/api/v1/public/upload-video', methods=['GET', 'POST'])
+def upload_video():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        model, device = load_model()
+        gps_info = extract_gps_data(file_path)
+        results = process_video(file_path, model, device, gps_info)
+        return results
+    else:
+        flash('File not allowed or missing')
+        return redirect(request.url)
+    
 if __name__ == '__main__':
     model, device = load_model()
     app.run(debug=True)
